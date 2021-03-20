b33539d82f925e667af0537b4f7caef7c3a75feb
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-03-20 11:33:48 -0700

    Initial Commit

---
+++ Plate Calculator WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plate Calculator WatchKit Extension
+//
+//  Created by Jose Aguilar on 3/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plate Calculator WatchKit Extension/Plate_CalculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  Plate_CalculatorApp.swift
+//  Plate Calculator WatchKit Extension
+//
+//  Created by Jose Aguilar on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Plate_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
